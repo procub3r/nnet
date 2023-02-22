eaf6ce3313ef1a5a98338076bdd55ec08272f1e5
procub3r <procub3r@protonmail.com>
2023-02-22 23:35:04 +0530

    Allow calling .autodiff() multiple times
    This only works if the expression to differentiate has been
    re-evaluated. If you call .autodiff() more than once in a row without
    re-evaluating the expression, you will not get the desired results.
    
    Also, there seems to be something wrong with using the result of an
    expression multiple times in another expression and differentiating with
    respect to this new expression. Gotta investigate this.

--- autodiff.py
+++ autodiff.py
@@ -5,47 +5,58 @@ class Scalar:
         self.value = value
         self.op = op
         self.children = children
-        self.dot = 0.0
+        self.__dot = 0.0
 
     def __repr__(self):
         return f'Scalar({self.value})'
 
+    @property
+    def dot(self):
+        return self.__dot
+
     def __add__(self, other):
+        self.__dot = other.__dot = 0.0
         return Scalar(self.value + other.value, op='+', children=(self, other))
 
     def __sub__(self, other):
+        self.__dot = other.__dot = 0.0
         return Scalar(self.value - other.value, op='-', children=(self, other))
 
     def __mul__(self, other):
+        self.__dot = other.__dot = 0.0
         return Scalar(self.value * other.value, op='*', children=(self, other))
 
     def __pow__(self, other):
+        self.__dot = other.__dot = 0.0
         return Scalar(self.value ** other.value, op='**', children=(self, other))
 
     def __truediv__(self, other):
+        self.__dot = other.__dot = 0.0
         return Scalar(self.value / other.value, op='/', children=(self, other))
 
-    def autodiff(self, inner=False):
+    def autodiff(self):
+        self.__dot = 1.0
+        self.__autodiff_rec()
+
+    def __autodiff_rec(self):
         if len(self.children) == 0:
             return
-        if not inner:
-            self.dot = 1.0
 
         if self.op == '+':
-            self.children[0].dot += 1.0 * self.dot
-            self.children[1].dot += 1.0 * self.dot
+            self.children[0].__dot += 1.0 * self.__dot
+            self.children[1].__dot += 1.0 * self.__dot
         elif self.op == '-':
-            self.children[0].dot += 1.0 * self.dot
-            self.children[1].dot += -1.0 * self.dot
+            self.children[0].__dot += 1.0 * self.__dot
+            self.children[1].__dot += -1.0 * self.__dot
         elif self.op == '*':
-            self.children[0].dot += self.children[1].value * self.dot
-            self.children[1].dot += self.children[0].value * self.dot
+            self.children[0].__dot += self.children[1].value * self.__dot
+            self.children[1].__dot += self.children[0].value * self.__dot
         elif self.op == '**':
-            self.children[0].dot += self.children[1].value * self.children[0].value ** (self.children[1].value - 1) * self.dot
-            self.children[1].dot += self.value * math.log(self.children[0].value) * self.dot
+            self.children[0].__dot += self.children[1].value * self.children[0].value ** (self.children[1].value - 1) * self.__dot
+            self.children[1].__dot += self.value * math.log(self.children[0].value) * self.__dot
         elif self.op == '/':
-            self.children[0].dot += self.dot / self.children[1].value
-            self.children[1].dot += self.children[0].value * -1.0 * self.children[1].value ** -2.0
+            self.children[0].__dot += self.__dot / self.children[1].value
+            self.children[1].__dot += self.children[0].value * -1.0 * self.children[1].value ** -2.0
 
-        self.children[0].autodiff(inner=True)
-        self.children[1].autodiff(inner=True)
+        for child in self.children:
+            child.__autodiff_rec()
